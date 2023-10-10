7319674b944052fde1c5aebfc922705a368d3700
Cross <67840383+Emerson-DVV@users.noreply.github.com>
2023-10-10 12:11:59 -0400

    funcionalidades nuevas "abre" "archivo" "escribe"

--- asistente_virtual.py
+++ asistente_virtual.py
@@ -1,5 +1,6 @@
 import speech_recognition as sr
-import pyttsx3, pywhatkit, wikipedia, datetime, keyboard
+import subprocess as sub
+import pyttsx3, pywhatkit, wikipedia, datetime, keyboard, os
 from pygame import mixer
 
 name = "Eva"
@@ -7,6 +8,19 @@ listener = sr.Recognizer()
 engine = pyttsx3.init()
 voices = engine.getProperty("voices")
 engine.setProperty("voice", voices[0].id)  # Se selecciona una voz en español
+
+sites = {
+    "google": "google.com",
+    "youtube": "youtube.com",
+    "facebook": "facebook.com",
+    "whatsapp": "web.whatsapp.com",
+    "cursos": "freecodecamp.org/learn",
+}
+
+files = {
+    "reconocimiento de voz": "Reconocimiento de Voz.pdf",
+    "sistema de antirobo": "SistemaAntiRobo.pdf",
+}
 
 
 def talk(text):
@@ -41,16 +55,18 @@ def run_eva():
         elif "busca" in rec:
             search = rec.replace("busca", "")
             wikipedia.set_lang("es")  # las busquedas realizadas seran en español
-            wiki = wikipedia.summary(search, 2) # summary resume la informacion el numero indica que tan detallado sera el resumen a mayor numero mayor resumen
+            wiki = wikipedia.summary(
+                search, 2
+            )  # summary resume la informacion el numero indica que tan detallado sera el resumen a mayor numero mayor resumen
             print(search + ": " + wiki)
             talk(wiki)
         elif "alarma" in rec:
-            num = rec.replace("alarma", '')
+            num = rec.replace("alarma", "")
             num = num.strip()
             print(num)
             talk("Alarma activada a las " + num + " horas")
             while True:
-                if datetime.datetime.now().strftime('%H:%M') == num:
+                if datetime.datetime.now().strftime("%H:%M") == num:
                     print("DESPIERTA::!!")
                     mixer.init()
                     mixer.music.load("Megumin.mp3")
@@ -58,6 +74,35 @@ def run_eva():
                     if keyboard.read_key() == "s":
                         mixer.music.stop()
                         break
+        elif "abre" in rec:
+            for site in sites:
+                if site in rec:
+                    sub.call(f"start chrome.exe {sites[site]}", shell=True)
+                    talk(f"Abriendo {site}")
+        elif "archivo" in rec:
+            for file in files:
+                if file in rec:
+                    sub.Popen([files[file]], shell=True)
+                    talk(f"Abriendo {file}")
+        elif "escribe" in rec:
+            try:
+                with open("nota.txt", "a") as f:
+                    write(f)
+            except FileNotFoundError as e:
+                file = open("nota.txt", "w")
+                write(file)
+        elif "adiós" in rec:
+            talk("adiós")
+            break
+
+
+def write(f):
+    talk("¿Qué quieres que escriba?")
+    rec_write = listen()
+    f.write(rec_write + os.linesep)
+    f.close()
+    talk("Lista, puedes revisarlo")
+    sub.Popen("nota.txt", shell=True)
 
 
 if __name__ == "__main__":
