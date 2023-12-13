9fc08196e36ed7d19894329a0ba7f0b9178d66c2
Ricardo <richardyord@gmail.com>
2023-12-13 11:53:43 -0400

    Coordenadas en un csv

--- interfazProyecto.py
+++ interfazProyecto.py
@@ -1,6 +1,7 @@
 from tkinter import *
 from PIL import ImageTk,Image
 from os import *
+import csv
 
 class interfazProyecto(Frame):
     def __init__(self, master=None):
@@ -8,15 +9,22 @@ class interfazProyecto(Frame):
         self.master = master
         self.pack()
         self.crearMapa()
+        self.coordenadas = []
         
     def crearMapa(self):
         carpetaImagen = path.dirname(__file__)
         rutaImagen = path.join(carpetaImagen,"imagen")
 
-        self.mapaCocha = ImageTk.PhotoImage(Image.open(path.join(rutaImagen,"mapaCocha.jpg")).resize((1000,700)))
+        self.mapaCocha = ImageTk.PhotoImage(Image.open(path.join(rutaImagen,"mapaCocha2.jpg")).resize((1000,700)))
         
         frameMapa = Frame(self)
         frameMapa.place(x=0,y=0,width=1000,height=700)
         
         mapa = Label(frameMapa,image=self.mapaCocha)
         mapa.place(x=0,y=0,width=1000,height=700)
+        
+        mapa.bind("<Button-1>", self.obtener_puntos)
+        
+    def obtener_puntos(self, event):
+        print( f"{event.x},{event.y}")
+
